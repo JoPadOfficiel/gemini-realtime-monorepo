7c9cbc8b2a6ba681e50e3fdb1390d145f7004619
JoPad Officiel <100805221+JoPadOfficiel@users.noreply.github.com>
2025-08-27 12:03:27 +0200

    fix: remove unused CTA register illustration

--- apps/web-app/components/sections/hero-landing.tsx
+++ apps/web-app/components/sections/hero-landing.tsx
@@ -35,7 +35,7 @@ export default async function HeroLanding() {
           )}
           target="_blank"
         >
-          <span className="mr-3">🚀</span>
+          <span className="mr-3">🎉</span>
           <span className="hidden md:flex">Powered by&nbsp;</span> Gemini Live
           AI on <Icons.twitter className="ml-2 size-3.5" />
         </Link>
